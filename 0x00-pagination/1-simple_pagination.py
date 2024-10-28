a82ec524f32f73b83ff625f242d080ebbae76bf0
Khalilu Rahman Mohammed <mohrahman558@gmail.com>
2024-10-28 23:19:19 +0000

    task 1 fix

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -32,6 +32,7 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Return the page of data"""
         assert isinstance(page, int) and isinstance(page_size, int)
         assert page > 0 and page_size > 0
         start_index, end_index = index_range(page, page_size)
