b6ee418dc2aa3c500a47635de47a3c9eb42ec3ed
Khalilu Rahman Mohammed <mohrahman558@gmail.com>
2024-10-30 22:02:17 +0000

    task 3 completed

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,39 @@
+#!/usr/bij/env python3
+"""LRU Cache"""
+
+BaseCaching = __import__("base_caching").BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """LRU Cache Implementation"""
+
+    def __init__(self):
+        super().__init__()
+        self.order = []
+
+    def put(self, key, item):
+        """Adds an item to the cache
+        based on LRU algorithm
+        """
+        if key is None or item is None:
+            return
+        size = len(self.cache_data)
+        if key not in self.cache_data and size >= BaseCaching.MAX_ITEMS:
+            recent = self.order.pop(-1)
+            self.cache_data.pop(recent)
+            print(f"DISCARD: {recent}")
+        self.cache_data[key] = item
+
+        if key in self.order:
+            self.order.remove(key)
+            self.order.insert(0, key)
+        else:
+            self.order.insert(0, key)
+
+    def get(self, key):
+        """Get an item by key"""
+        if key is None or self.cache_data.get(key) is None:
+            return None
+        self.order.remove(key)
+        self.order.insert(0, key)
+        return self.cache_data.get(key)
