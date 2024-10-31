fa35986e6d17b6beb0ae4c9859bd34a10551920b
Khalilu Rahman Mohammed <mohrahman558@gmail.com>
2024-10-31 07:34:35 +0000

    hot fixes

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""LRU Cache"""
+"""Implementation for the LRU Cache"""
 
 BaseCaching = __import__("base_caching").BaseCaching
 
--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""MRU Cache"""
+"""Implemenatation for the MRU Cache"""
 
 BaseCaching = __import__("base_caching").BaseCaching
 
